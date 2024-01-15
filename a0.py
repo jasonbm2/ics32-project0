85e1303304262d4bd91fa4e104c3b1fc218f8846
Jason Martin <jasonbm2@uci.edu>
2024-01-15 13:37:05 -0800

    began loop inside draw function

--- a0.py
+++ a0.py
@@ -1,6 +1,9 @@
 def draw():
     blocks = int(input())
-    
+    for n in range(blocks + 1):
+        print(" " * n * 2 + "+-+")
+        print(" " * n * 2 + "  " + "| |")
+        print(" " * n * 2 + "    " + "+-", end="")
 
 def main():
     draw()
